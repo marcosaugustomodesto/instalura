9454b06bc22806badacf99fcb28550e4dd97378c
Modesto <msmd@gft.com>
2019-04-22 11:39:09 -0300

    aula 1

---
+++ src/componentes/Timeline.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import FotoItem from './FotoItem';
+
+export default class Timeline extends Component {
+    render(){
+        return (
+        <div className="fotos container">
+          <FotoItem/>
+        </div>            
+        );
+    }
+}
