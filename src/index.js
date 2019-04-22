80e220980b0fa5d73608ab9d3fbc3ff49abc4523
Modesto <msmd@gft.com>
2019-04-22 18:33:45 -0300

    aula 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './componentes/Login';
 import './css/reset.css';
 import './css/timeline.css';
+import './css/login.css';
+import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+	<Router history={history}>
+        <Switch>        
+            <Route exact path="/" component={Login}/>
+            <Route path="/timeline" component={App}/>
+        </Switch>            		
+	</Router>,	
+	document.getElementById('root')
+);
+
